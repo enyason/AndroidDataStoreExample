74cc377adc67c6e326190aef41d1e7374ea0a670
Emmanuel Enya <enyason@Emmanuels-MacBook-Pro.local>
2021-12-06 12:54:13 +0100

    Initial commit

---
+++ app/src/main/java/com/enyason/androiddatastoreexample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.enyason.androiddatastoreexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
